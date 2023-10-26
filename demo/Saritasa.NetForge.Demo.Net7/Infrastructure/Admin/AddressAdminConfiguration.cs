97469777c913834b3f593b57197fcb4de0bc9329
Vasily Chefonov <vasily.chefonov@saritasa.com>
2023-10-26 15:22:02 +0700

    Provide Address admin configuration class SN-431

---
+++ demo/Saritasa.NetForge.Demo.Net7/Infrastructure/Admin/AddressAdminConfiguration.cs
@@ -0,0 +1,37 @@
+﻿using Saritasa.NetForge.Demo.Net7.Models;
+using Saritasa.NetForge.DomainServices;
+using Saritasa.NetForge.DomainServices.Interfaces;
+
+namespace Saritasa.NetForge.Demo.Net7.Infrastructure.Admin;
+
+/// <summary>
+/// <see cref="Address"/> admin panel configuration.
+/// </summary>
+public class AddressAdminConfiguration : IEntityAdminConfiguration<Address>
+{
+    /// <inheritdoc />
+    public void Configure(EntityOptionsBuilder<Address> entityOptionsBuilder)
+    {
+        entityOptionsBuilder.ConfigureProperty(address => address.Id, propertyBuilder =>
+        {
+            propertyBuilder
+                .SetDescription("Item identifier.")
+                .SetOrder(2);
+        }).ConfigureProperty(address => address.ContactPhone, propertyBuilder =>
+        {
+            propertyBuilder
+                .SetDisplayName("Phone")
+                .SetDescription("Address contact phone.")
+                .SetOrder(1);
+        }).ConfigureProperty(address => address.PostalCode, propertyBuilder =>
+        {
+            propertyBuilder.SetIsHidden(true);
+        }).ConfigureProperty(address => address.City, propertyBuilder =>
+        {
+            propertyBuilder.SetDisplayName("Town");
+        }).ConfigureProperty(address => address.Longitude, propertyBuilder =>
+        {
+            propertyBuilder.SetOrder(6);
+        });
+    }
+}
