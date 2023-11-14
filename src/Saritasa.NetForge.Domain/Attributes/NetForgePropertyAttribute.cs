bc7d34b17f6985612575ccd9c7347bb6e1752ba1
Alexander Makarov <alexandr.makarov@saritasa.com>
2023-11-14 15:32:04 +0700

    Add base property attribute SN-462

--- src/Saritasa.NetForge.Domain/Attributes/NetForgePropertyAttribute.cs
+++ src/Saritasa.NetForge.Domain/Attributes/NetForgePropertyAttribute.cs
@@ -7,27 +7,8 @@ namespace Saritasa.NetForge.Domain.Attributes;
 /// Represents an attribute used to provide metadata for the entity's property.
 /// </summary>
 [AttributeUsage(AttributeTargets.Property)]
-public class NetForgePropertyAttribute : Attribute
+public class NetForgePropertyAttribute : NetForgePropertyAttributeBase
 {
-    /// <inheritdoc cref="PropertyMetadata.IsHidden"/>
-    public bool IsHidden { get; set; }
-
-    /// <inheritdoc cref="PropertyMetadata.DisplayName"/>
-    public string? DisplayName { get; set; }
-
-    /// <inheritdoc cref="PropertyMetadata.Description"/>
-    public string? Description { get; set; }
-
-    /// <inheritdoc cref="PropertyMetadata.Order"/>
-    /// <remarks>
-    /// We override default value with <c>-1</c>,
-    /// because we need to handle situation when user chose property order is <c>0</c>.
-    /// </remarks>
-    public int Order { get; set; } = -1;
-
-    /// <inheritdoc cref="PropertyMetadata.DisplayFormat"/>
-    public string? DisplayFormat { get; set; }
-
     /// <inheritdoc cref="PropertyMetadata.SearchType"/>
     public SearchType SearchType { get; set; } = SearchType.None;
 
