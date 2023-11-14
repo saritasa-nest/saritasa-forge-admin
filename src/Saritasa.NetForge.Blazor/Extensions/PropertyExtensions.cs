a6b22afe18082d2affd09fed71a5acfd001b4dcd
Toan Pham <toan.pham@saritasa.com>
2023-11-14 13:47:56 +0700

    Replace the regex to correctly match the pattern SN-445

--- src/Saritasa.NetForge.Blazor/Extensions/PropertyExtensions.cs
+++ src/Saritasa.NetForge.Blazor/Extensions/PropertyExtensions.cs
@@ -14,6 +14,6 @@ public static class PropertyExtensions
     public static string ToMeaningfulName(this string value)
     {
         // Using regex to add a space at the beginning of every single uppercase letter except the beginning of the word.
-        return Regex.Replace(value, "(?!^)([A-Z])", " $1");
+        return Regex.Replace(value, "([A-Z])([A-Z])([a-z])|([a-z])([A-Z])", "$1$4 $2$3$5");
     }
 }
