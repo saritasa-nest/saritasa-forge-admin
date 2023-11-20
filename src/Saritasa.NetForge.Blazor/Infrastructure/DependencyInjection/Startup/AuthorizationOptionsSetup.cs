8b95cdd64eee3dddac92237321082e4b39e609dd
Hieu Nguyen <tanhieu.nguyen@saritasa.com>
2023-11-20 12:37:49 +0700

    Add policy for custom role and function access to admin panel SN-432

---
+++ src/Saritasa.NetForge.Blazor/Infrastructure/DependencyInjection/Startup/AuthorizationOptionsSetup.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Authorization;
+using Saritasa.NetForge.Blazor.Infrastructure.Authentication;
+using Saritasa.NetForge.Domain.Entities.Options;
+
+namespace Saritasa.NetForge.Blazor.Infrastructure.DependencyInjection.Startup;
+
+/// <summary>
+/// Authorization options setup for admin panel.
+/// </summary>
+public class AuthorizationOptionsSetup
+{
+    private readonly AdminOptions adminOptions;
+
+    /// <summary>
+    /// Constructor.
+    /// </summary>
+    /// <param name="adminOptions">Admin options.</param>
+    public AuthorizationOptionsSetup(AdminOptions adminOptions)
+    {
+        this.adminOptions = adminOptions;
+    }
+
+    /// <summary>
+    /// Setup authorization.
+    /// </summary>
+    /// <param name="options">Authorization options.</param>
+    public void Setup(AuthorizationOptions options)
+    {
+        options.AddPolicy("AdminPanelAccess", policy =>
+        {
+            if (adminOptions.AdminPanelAccessRoles.Any())
+            {
+                policy.RequireRole(adminOptions.AdminPanelAccessRoles);
+            }
+
+            if (adminOptions.CustomAuthFunction != null)
+            {
+                policy.AddRequirements(new CustomAuthFunctionRequirement(adminOptions.CustomAuthFunction));
+            }
+        });
+    }
+}
