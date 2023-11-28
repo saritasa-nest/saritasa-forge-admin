9c8fb7fa172201a5700d5e6f05f4120227c36ff5
Alexander Makarov <alexandr.makarov@saritasa.com>
2023-11-28 16:30:30 +0700

    Add support of datetime SN-521

--- src/Saritasa.NetForge.Mvvm/ViewModels/CreateEntity/CreateEntityModel.cs
+++ src/Saritasa.NetForge.Mvvm/ViewModels/CreateEntity/CreateEntityModel.cs
@@ -14,6 +14,9 @@ public class CreateEntityModel
     /// <inheritdoc cref="EntityMetadata.DisplayName"/>
     public string DisplayName { get; set; } = string.Empty;
 
+    /// <inheritdoc cref="EntityMetadata.ClrType"/>
+    public Type? ClrType { get; set; }
+
     /// <inheritdoc cref="EntityMetadata.Properties"/>
     public ICollection<PropertyMetadataDto> Properties { get; set; } = new List<PropertyMetadataDto>();
 }
