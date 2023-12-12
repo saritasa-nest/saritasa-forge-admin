9f02b71acddaaa11f5545d12e5b8792947cd2a0c
Vasily Chefonov <vasily.chefonov@saritasa.com>
2023-12-12 19:20:01 +0700

    Retarget demo to NET 8

---
+++ demo/Saritasa.NetForge.Demo/Infrastructure/Startup/IdentityOptionsSetup.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Saritasa.NetForge.Demo.Infrastructure.Startup;
+
+/// <summary>
+/// Identity options setup.
+/// </summary>
+public class IdentityOptionsSetup
+{
+    /// <summary>
+    /// Setup identity.
+    /// </summary>
+    /// <param name="options">The options.</param>
+    public void Setup(IdentityOptions options)
+    {
+        options.User.RequireUniqueEmail = true;
+        options.Password.RequireNonAlphanumeric = false;
+    }
+}
---
+++ demo/Saritasa.NetForge.Demo/Models/Product.cs
@@ -0,0 +1,80 @@
+﻿using Saritasa.NetForge.Demo.Constants;
+using Saritasa.NetForge.Domain.Attributes;
+
+namespace Saritasa.NetForge.Demo.Models;
+
+/// <summary>
+/// Represents a product entity.
+/// </summary>
+[NetForgeEntity(Description = "The product in the shop.", GroupName = GroupConstants.Shops)]
+public class Product
+{
+    /// <summary>
+    /// The unique identifier for the product.
+    /// </summary>
+    public int Id { get; set; }
+
+    /// <summary>
+    /// The name of the product.
+    /// </summary>
+    public required string Name { get; set; }
+
+    /// <summary>
+    /// The description of the product.
+    /// </summary>
+    public required string Description { get; set; }
+
+    /// <summary>
+    /// The price of the product.
+    /// </summary>
+    [NetForgeProperty(DisplayFormat = "{0:C}")]
+    public decimal Price { get; set; }
+
+    /// <summary>
+    /// The quantity of the product in stock.
+    /// </summary>
+    public int StockQuantity { get; set; }
+
+    /// <summary>
+    /// The weight of the product in grams.
+    /// </summary>
+    public float WeightInGrams { get; set; }
+
+    /// <summary>
+    /// The length of the product in centimeters.
+    /// </summary>
+    public float LengthInCentimeters { get; set; }
+
+    /// <summary>
+    /// The width of the product in centimeters.
+    /// </summary>
+    public float WidthInCentimeters { get; set; }
+
+    /// <summary>
+    /// The height of the product in centimeters.
+    /// </summary>
+    public float HeightInCentimeters { get; set; }
+
+    /// <summary>
+    /// The date and time when the product was created.
+    /// </summary>
+    public DateTime CreatedDate { get; set; }
+
+    /// <summary>
+    /// The date and time when the product was last updated.
+    /// </summary>
+    public DateTime UpdatedDate { get; set; }
+
+    /// <summary>
+    /// The list of tags associated with the product.
+    /// </summary>
+    [NetForgeNavigation(IsIncluded = true)]
+    public List<ProductTag> Tags { get; set; } = new();
+
+    /// <summary>
+    /// Supplier that providing this product.
+    /// </summary>
+    [NetForgeNavigation(IsIncluded = true)]
+    public required Supplier Supplier { get; set; }
+}
+
