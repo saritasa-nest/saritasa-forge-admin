e8e900d68ee707d2a750abe0c4ab51231b6cbb45
Toan Pham <toan.pham@saritasa.com>
2023-12-18 08:13:29 +0700

    Fix mistake in comment SN-519

--- src/Saritasa.NetForge.Domain/Attributes/NetForgePropertyAttributeBase.cs
+++ src/Saritasa.NetForge.Domain/Attributes/NetForgePropertyAttributeBase.cs
@@ -32,6 +32,6 @@ public abstract class NetForgePropertyAttributeBase : Attribute
     /// <inheritdoc cref="PropertyMetadataBase.EmptyValueDisplay"/>
     public string EmptyValueDisplay { get; set; } = string.Empty;
 
-    /// <inheritdoc cref="PropertyMetadata.HtmlTemplate"/>
+    /// <inheritdoc cref="PropertyMetadataBase.HtmlTemplate"/>
     public string HtmlTemplate { get; set; } = string.Empty;
 }
