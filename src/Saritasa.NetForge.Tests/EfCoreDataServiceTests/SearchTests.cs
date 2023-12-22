cff81ef52e15aff62e10d020384e49923a6941ad
Alexander Makarov <alexandr.makarov@saritasa.com>
2023-12-22 12:28:52 +0700

    Move populating database with test data to tests class from fixture SN-422

--- src/Saritasa.NetForge.Tests/EfCoreDataServiceTests/SearchTests.cs
+++ src/Saritasa.NetForge.Tests/EfCoreDataServiceTests/SearchTests.cs
@@ -27,6 +27,68 @@ public class SearchTests : TestBed<TestDatabaseFixture>
     {
         testDbContext = testDatabaseFixture.TestDbContext;
         dataService = testDatabaseFixture.GetScopedService<IOrmDataService>(testOutputHelper)!;
+
+        PopulateDatabaseWithTestData();
+    }
+
+    private void PopulateDatabaseWithTestData()
+    {
+        if (testDbContext.Addresses.Any())
+        {
+            return;
+        }
+
+        testDbContext.Addresses.Add(new Address
+        {
+            Street = "Main St.",
+            City = "New York",
+            Latitude = 100
+        });
+        testDbContext.Addresses.Add(new Address
+        {
+            Street = "Main Square St.",
+            City = "London",
+            Latitude = 101
+        });
+        testDbContext.Addresses.Add(new Address
+        {
+            Street = "Second Square St.",
+            City = "London",
+            Latitude = 102
+        });
+        testDbContext.Addresses.Add(new Address
+        {
+            Street = "Second main St.",
+            City = "New York",
+            Latitude = 10
+        });
+        testDbContext.Addresses.Add(new Address
+        {
+            Street = "Central",
+            City = "London",
+            Latitude = 222
+        });
+        testDbContext.Addresses.Add(new Address
+        {
+            Street = "central street",
+            City = "New York",
+            Latitude = 1
+        });
+
+        testDbContext.Products.Add(new Product
+        {
+            Supplier = new Supplier()
+        });
+        testDbContext.Products.Add(new Product
+        {
+            WeightInGrams = 111,
+            Supplier = new Supplier
+            {
+                Name = "Supplier",
+                City = "London"
+            }
+        });
+        testDbContext.SaveChanges();
     }
 
     /// <summary>
--- src/Saritasa.NetForge.Tests/Utilities/TestDatabaseFixture.cs
+++ src/Saritasa.NetForge.Tests/Utilities/TestDatabaseFixture.cs
@@ -5,7 +5,6 @@ using Saritasa.NetForge.Blazor.Extensions;
 using Saritasa.NetForge.Blazor.Infrastructure.DependencyInjection;
 using Saritasa.NetForge.Infrastructure.EfCore.Extensions;
 using Saritasa.NetForge.Tests.Domain;
-using Saritasa.NetForge.Tests.Domain.Models;
 using Xunit.Microsoft.DependencyInjection;
 using Xunit.Microsoft.DependencyInjection.Abstracts;
 
@@ -16,7 +15,7 @@ namespace Saritasa.NetForge.Tests.Utilities;
 /// </summary>
 /// <remarks>
 /// Use case:
-/// When we want the same database state for all tests in one class.
+/// When we want the same database state for all tests.
 /// For example, when we don't modify any data in the database.
 /// </remarks>
 public class TestDatabaseFixture : TestBedFixture
@@ -35,68 +34,11 @@ public class TestDatabaseFixture : TestBedFixture
         var testDbContext = new TestDbContext(dbOptions);
         testDbContext.Database.EnsureCreated();
 
-        testDbContext.Addresses.Add(new Address
-        {
-            Street = "Main St.",
-            City = "New York",
-            Latitude = 100
-        });
-        testDbContext.Addresses.Add(new Address
-        {
-            Street = "Main Square St.",
-            City = "London",
-            Latitude = 101
-        });
-        testDbContext.Addresses.Add(new Address
-        {
-            Street = "Second Square St.",
-            City = "London",
-            Latitude = 102
-        });
-        testDbContext.Addresses.Add(new Address
-        {
-            Street = "Second main St.",
-            City = "New York",
-            Latitude = 10
-        });
-        testDbContext.Addresses.Add(new Address
-        {
-            Street = "Central",
-            City = "London",
-            Latitude = 222
-        });
-        testDbContext.Addresses.Add(new Address
-        {
-            Street = "central street",
-            City = "New York",
-            Latitude = 1
-        });
-
-        testDbContext.Products.Add(new Product
-        {
-            Supplier = new Supplier()
-        });
-        testDbContext.Products.Add(new Product
-        {
-            WeightInGrams = 111,
-            Supplier = new Supplier
-            {
-                Name = "Supplier",
-                City = "London"
-            }
-        });
-        testDbContext.SaveChanges();
-
         TestDbContext = testDbContext;
     }
 
     /// <inheritdoc />
-    protected override ValueTask DisposeAsyncCore()
-    {
-        TestDbContext.Dispose();
-
-        return default;
-    }
+    protected override ValueTask DisposeAsyncCore() => default;
 
     /// <inheritdoc />
     protected override void AddServices(IServiceCollection services, IConfiguration? configuration)
