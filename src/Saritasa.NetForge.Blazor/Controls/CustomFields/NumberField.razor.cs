01a60c02c67bf4212765276628b6af53f0487b12
Alexander Makarov <alexandr.makarov@saritasa.com>
2024-01-09 16:34:34 +0700

    Refactor number type add using of dynamic component SN-539

---
+++ src/Saritasa.NetForge.Blazor/Controls/CustomFields/NumberField.razor.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Components;
+using Saritasa.NetForge.UseCases.Metadata.GetEntityById;
+
+namespace Saritasa.NetForge.Blazor.Controls.CustomFields;
+
+/// <summary>
+/// Represents number field.
+/// </summary>
+public partial class NumberField<T>
+{
+    /// <summary>
+    /// Property name.
+    /// </summary>
+    [Parameter]
+    [EditorRequired]
+    public PropertyMetadataDto Property { get; init; } = null!;
+
+    /// <summary>
+    /// Entity model that contains property value for this field.
+    /// </summary>
+    [Parameter]
+    [EditorRequired]
+    public object EntityModel { get; init; } = null!;
+
+    /// <summary>
+    /// Property date value.
+    /// </summary>
+    public T? PropertyValue
+    {
+        get => (T?)EntityModel.GetType().GetProperty(Property.Name)?.GetValue(EntityModel);
+        set => EntityModel.GetType().GetProperty(Property.Name)?.SetValue(EntityModel, value);
+    }
+}
