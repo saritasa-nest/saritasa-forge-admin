8ec43e6be8357c309f35f0461582408ae2f41ef7
Alexander Makarov <alexandr.makarov@saritasa.com>
2024-01-11 13:01:54 +0700

    Add generated property to contact info SN-540

---
+++ demo/Saritasa.NetForge.Demo/Migrations/20240111055616_AddGeneratedPropertyToContactInfo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Saritasa.NetForge.Demo.Net7.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddGeneratedPropertyToContactInfo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "created_at",
+                table: "contact_infos",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "created_at",
+                table: "contact_infos");
+        }
+    }
+}
