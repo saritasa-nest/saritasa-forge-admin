99acb0cb0ad51e29aa0bd30614709f1b52473693
Toan Pham <toan.pham@saritasa.com>
2024-01-19 17:31:50 +0700

    Add delete confirmation dialog SN-522

---
+++ src/Saritasa.NetForge.Blazor/Controls/DeleteEntityConfirmationDialog.razor.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace Saritasa.NetForge.Blazor.Controls;
+
+/// <summary>
+/// Delete entity confirmation dialog.
+/// </summary>
+public partial class DeleteEntityConfirmationDialog
+{
+    [CascadingParameter]
+    private MudDialogInstance MudDialog { get; set; } = default!;
+
+    private void Submit() => MudDialog.Close(DialogResult.Ok(true));
+
+    private void Cancel() => MudDialog.Cancel();
+}
