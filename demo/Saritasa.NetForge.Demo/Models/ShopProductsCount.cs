3feec3e3ea1a330b4abe48942e73291be56144df
Alexandr Makarov <Alexandr.Makarov@saritasa.com>
2024-01-31 17:30:33 +0700

    Add keyless entity SN-566

---
+++ demo/Saritasa.NetForge.Demo/Models/ShopProductsCount.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Saritasa.NetForge.Demo.Models;
+
+/// <summary>
+/// Count of products in the shop.
+/// </summary>
+[Keyless]
+public class ShopProductsCount
+{
+    /// <summary>
+    /// Shop.
+    /// </summary>
+    public required Shop Shop { get; init; }
+
+    /// <summary>
+    /// Products count.
+    /// </summary>
+    public int ProductsCount { get; init; }
+}
