133f3a590c49fba8d203dfbafbb9e2474fc9ecfb
Alexandr Makarov <Alexandr.Makarov@saritasa.com>
2024-02-01 11:10:27 +0700

    Configure shop product count entity SN-566

--- demo/Saritasa.NetForge.Demo/Models/ShopProductsCount.cs
+++ demo/Saritasa.NetForge.Demo/Models/ShopProductsCount.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Saritasa.NetForge.Demo.Constants;
+using Saritasa.NetForge.Domain.Attributes;
 
 namespace Saritasa.NetForge.Demo.Models;
 
@@ -6,15 +8,18 @@ namespace Saritasa.NetForge.Demo.Models;
 /// Count of products in the shop.
 /// </summary>
 [Keyless]
+[NetForgeEntity(GroupName = GroupConstants.Shops)]
 public class ShopProductsCount
 {
     /// <summary>
     /// Shop.
     /// </summary>
+    [NetForgeNavigation(IsIncluded = true, Order = 1, DisplayName = "Shop Id")]
     public required Shop Shop { get; init; }
 
     /// <summary>
     /// Products count.
     /// </summary>
+    [NetForgeProperty(Order = 2)]
     public int ProductsCount { get; init; }
 }
