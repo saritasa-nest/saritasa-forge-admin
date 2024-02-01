464ce11e1f69f34d82612b76689551bf815d8b6e
Alexandr Makarov <Alexandr.Makarov@saritasa.com>
2024-02-01 14:28:44 +0700

    Add tests for group headers expanded admin option SN-581

---
+++ src/Saritasa.NetForge.Tests/AdminOptionsTests.cs
@@ -0,0 +1,59 @@
+﻿using Saritasa.NetForge.Domain.Entities.Options;
+using Saritasa.NetForge.DomainServices;
+using Xunit;
+
+namespace Saritasa.NetForge.Tests;
+
+/// <summary>
+/// <see cref="AdminOptions"/> tests.
+/// </summary>
+public class AdminOptionsTests
+{
+    /// <summary>
+    /// Verify that entity group headers are expanded when configured.
+    /// </summary>
+    [Fact]
+    public void AdminOptions_ConfigureGroupHeadersToBeExpanded_ShouldBeExpanded()
+    {
+        // Arrange
+        var adminOptionsBuilder = new AdminOptionsBuilder();
+        var adminOptions = adminOptionsBuilder.Create();
+
+        // Act
+        adminOptionsBuilder.SetGroupHeadersExpanded(true);
+
+        // Assert
+        Assert.True(adminOptions.GroupHeadersExpanded);
+    }
+
+    /// <summary>
+    /// Verify that entity group headers are not expanded when configured.
+    /// </summary>
+    [Fact]
+    public void AdminOptions_ConfigureGroupHeadersToBeNotExpanded_ShouldNotBeExpanded()
+    {
+        // Arrange
+        var adminOptionsBuilder = new AdminOptionsBuilder();
+        var adminOptions = adminOptionsBuilder.Create();
+
+        // Act
+        adminOptionsBuilder.SetGroupHeadersExpanded(false);
+
+        // Assert
+        Assert.False(adminOptions.GroupHeadersExpanded);
+    }
+
+    /// <summary>
+    /// Verify that entity group headers are expanded by default.
+    /// </summary>
+    [Fact]
+    public void AdminOptions_GroupHeadersAreNotConfigured_ShouldBeExpanded()
+    {
+        // Arrange & Act
+        var adminOptionsBuilder = new AdminOptionsBuilder();
+        var adminOptions = adminOptionsBuilder.Create();
+
+        // Assert
+        Assert.True(adminOptions.GroupHeadersExpanded);
+    }
+}
