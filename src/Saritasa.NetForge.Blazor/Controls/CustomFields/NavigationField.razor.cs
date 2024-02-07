0c0d69de69c5dd383d41c5f48a51254c73f5fabb
Alexandr Makarov <Alexandr.Makarov@saritasa.com>
2024-02-07 16:56:02 +0700

    Create navigation field SN-563

---
+++ src/Saritasa.NetForge.Blazor/Controls/CustomFields/NavigationField.razor.cs
@@ -0,0 +1,16 @@
+﻿namespace Saritasa.NetForge.Blazor.Controls.CustomFields;
+
+/// <summary>
+/// Field for navigation property.
+/// </summary>
+public partial class NavigationField : CustomField
+{
+    /// <summary>
+    /// Property value.
+    /// </summary>
+    public object? PropertyValue
+    {
+        get => EntityInstance.GetType().GetProperty(Property.Name)?.GetValue(EntityInstance);
+        set => EntityInstance.GetType().GetProperty(Property.Name)?.SetValue(EntityInstance, value);
+    }
+}
