b699068374890f53ccc3fe611606180cdc5f0361
Vasily Chefonov <vasily.chefonov@saritasa.com>
2024-02-19 16:47:06 +0700

    Add the CKEditor field SN-611

---
+++ src/Saritasa.NetForge.Blazor/Controls/CustomFields/CKEditorField.razor.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace Saritasa.NetForge.Blazor.Controls.CustomFields;
+
+/// <summary>
+/// Represents CKEditor field.
+/// </summary>
+public partial class CKEditorField : CustomField, IAsyncDisposable
+{
+    private IJSObjectReference? jsModule;
+    private ElementReference editor;
+
+    /// <inheritdoc />
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (firstRender)
+        {
+            await base.OnAfterRenderAsync(firstRender);
+            jsModule = await JsRuntime.InvokeAsync<IJSObjectReference>("import",
+                "./_content/NetForgeBlazor/Controls/CustomFields/CKEditorField.razor.js");
+            await jsModule.InvokeVoidAsync("InitCKEditor", editor);
+        }
+    }
+
+    /// <summary>
+    /// Property value.
+    /// </summary>
+    public string? PropertyValue
+    {
+        get => EntityInstance.GetType().GetProperty(Property.Name)?.GetValue(EntityInstance)?.ToString();
+        set => EntityInstance.GetType().GetProperty(Property.Name)?.SetValue(EntityInstance, value);
+    }
+
+    /// <inheritdoc />
+    public async ValueTask DisposeAsync()
+    {
+        GC.SuppressFinalize(this);
+
+        if (jsModule is not null)
+        {
+            await jsModule.DisposeAsync();
+        }
+    }
+}
---
+++ src/Saritasa.NetForge.Blazor/Controls/CustomFields/CKEditorField.razor.js
@@ -0,0 +1,9 @@
+﻿import "/_content/NetForgeBlazor/ckeditor.js";
+
+/**
+ * Initializes the CKEditor instance on the given element.
+ * @param {HTMLElement} element - The HTML element where the CKEditor should be initialized.
+ */
+export function InitCKEditor(element) {
+    ClassicEditor.create(element);
+}
