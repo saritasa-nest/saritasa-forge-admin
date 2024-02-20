ff6448e76530d6242f38b2e35d99390d6331be35
Toan Pham <toan.pham@saritasa.com>
2024-02-20 10:09:55 +0700

    Update test for create existing entity SN-522

--- src/Saritasa.NetForge.Tests/EfCoreDataServiceTests/CreateEntityTests.cs
+++ src/Saritasa.NetForge.Tests/EfCoreDataServiceTests/CreateEntityTests.cs
@@ -57,15 +57,12 @@ public class CreateEntityTests : TestBed<NetForgeFixture>
     public async Task CreateEntity_AlreadyExistingEntity_Error()
     {
         // Arrange
-        var contactInfos = Fakers.ContactInfoFaker.Generate(2);
-        testDbContext.ContactInfos.AddRange(contactInfos);
-        await testDbContext.SaveChangesAsync(CancellationToken.None);
-        var contactInfoType = typeof(ContactInfo);
         var contactInfo = Fakers.ContactInfoFaker.Generate();
-        contactInfo.Id = 1;
+        testDbContext.ContactInfos.Add(contactInfo);
+        await testDbContext.SaveChangesAsync(CancellationToken.None);
 
         // Act
-        async Task Act() => await efCoreDataService.AddAsync(contactInfo, contactInfoType, CancellationToken.None);
+        async Task Act() => await efCoreDataService.AddAsync(contactInfo, typeof(ContactInfo), CancellationToken.None);
 
         // Assert
         await Assert.ThrowsAnyAsync<Exception>(Act);
