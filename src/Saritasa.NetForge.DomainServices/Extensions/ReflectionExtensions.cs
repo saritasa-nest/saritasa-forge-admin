0bb77b465fd18f8603dfb386da62fbead18a5719
Alexandr Makarov <Alexandr.Makarov@saritasa.com>
2024-02-29 18:18:00 +0700

    Add reflection extensions with get property value from instance method SN-563

---
+++ src/Saritasa.NetForge.DomainServices/Extensions/ReflectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace Saritasa.NetForge.DomainServices.Extensions;
+
+/// <summary>
+/// Contains methods that work with reflection.
+/// </summary>
+public static class ReflectionExtensions
+{
+    /// <summary>
+    /// Gets value of <paramref name="obj"/> property with given name.
+    /// </summary>
+    public static object? GetPropertyValue(this object? obj, string propertyName)
+    {
+        return obj?.GetType().GetProperty(propertyName)?.GetValue(obj);
+    }
+}
