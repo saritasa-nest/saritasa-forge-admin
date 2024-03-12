67e7845546ff41b84a875c5a68b0b8600308a4f6
Alexandr Makarov <Alexandr.Makarov@saritasa.com>
2024-03-12 18:20:33 +0700

    Add truncate method SN-615

---
+++ src/Saritasa.NetForge.DomainServices/Extensions/StringExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace Saritasa.NetForge.DomainServices.Extensions;
+
+/// <summary>
+/// <see cref="string"/> extensions.
+/// </summary>
+public static class StringExtensions
+{
+    /// <summary>
+    /// Truncates <paramref name="value"/>.
+    /// </summary>
+    /// <param name="value">String to truncate.</param>
+    /// <param name="maxChars">Maximum characters.</param>
+    /// <returns>Truncated string.</returns>
+    public static string Truncate(this string value, int maxChars)
+    {
+        return value.Length <= maxChars ? value : $"{value[..maxChars]}...";
+    }
+}
