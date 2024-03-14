55c16eedb30669e1b6e84e562fe65a9c4e3d5b58
Alexandr Makarov <Alexandr.Makarov@saritasa.com>
2024-03-14 14:35:59 +0700

    Change max characters parameter name of truncate method SN-615

--- src/Saritasa.NetForge.DomainServices/Extensions/StringExtensions.cs
+++ src/Saritasa.NetForge.DomainServices/Extensions/StringExtensions.cs
@@ -9,10 +9,10 @@ public static class StringExtensions
     /// Truncates <paramref name="value"/>.
     /// </summary>
     /// <param name="value">String to truncate.</param>
-    /// <param name="maxChars">Maximum characters.</param>
+    /// <param name="maxCharacters">Maximum characters.</param>
     /// <returns>Truncated string.</returns>
-    public static string Truncate(this string value, int maxChars)
+    public static string Truncate(this string value, int maxCharacters)
     {
-        return value.Length <= maxChars ? value : $"{value[..maxChars]}...";
+        return value.Length <= maxCharacters ? value : $"{value[..maxCharacters]}...";
     }
 }
