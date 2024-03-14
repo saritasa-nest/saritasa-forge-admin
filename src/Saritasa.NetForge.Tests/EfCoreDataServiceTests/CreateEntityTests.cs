21ec12e66090740bc3ebd70c85953e3f2369ea60
Alexandr Makarov <Alexandr.Makarov@saritasa.com>
2024-03-14 17:14:27 +0700

    Add isolation to create entity tests SN-633

--- src/Saritasa.NetForge.Tests/EfCoreDataServiceTests/CreateEntityTests.cs
+++ src/Saritasa.NetForge.Tests/EfCoreDataServiceTests/CreateEntityTests.cs
@@ -1,11 +1,7 @@
 ﻿using Saritasa.NetForge.Infrastructure.Abstractions.Interfaces;
 using Saritasa.NetForge.Tests.Domain;
-using Saritasa.NetForge.Tests.Fixtures;
 using Saritasa.NetForge.Tests.Helpers;
 using Xunit;
-using Xunit.Abstractions;
-using Xunit.Microsoft.DependencyInjection.Abstracts;
-using Xunit.Microsoft.DependencyInjection.Attributes;
 using ContactInfo = Saritasa.NetForge.Tests.Domain.Models.ContactInfo;
 
 namespace Saritasa.NetForge.Tests.EfCoreDataServiceTests;
@@ -13,29 +9,52 @@ namespace Saritasa.NetForge.Tests.EfCoreDataServiceTests;
 /// <summary>
 /// Create entity tests.
 /// </summary>
-[TestCaseOrderer(Constants.OrdererTypeName, Constants.OrdererAssemblyName)]
-public class CreateEntityTests : TestBed<NetForgeFixture>
+public class CreateEntityTests : IDisposable
 {
-#pragma warning disable CA2213
     private readonly TestDbContext testDbContext;
-#pragma warning restore CA2213
     private readonly IOrmDataService efCoreDataService;
 
     /// <summary>
     /// Constructor.
     /// </summary>
-    public CreateEntityTests(ITestOutputHelper testOutputHelper, NetForgeFixture netForgeFixture)
-        : base(testOutputHelper, netForgeFixture)
+    public CreateEntityTests()
     {
-        testDbContext = _fixture.GetService<TestDbContext>(_testOutputHelper)!;
-        efCoreDataService = _fixture.GetService<IOrmDataService>(_testOutputHelper)!;
+        testDbContext = EfCoreHelper.CreateTestDbContext();
+        efCoreDataService = EfCoreHelper.CreateEfCoreDataService(testDbContext);
+    }
+
+    private bool disposedValue;
+
+    /// <inheritdoc />
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    /// <summary>
+    /// Deletes the database after one test is complete,
+    /// so it gives us the same state of the database for every test.
+    /// </summary>
+    protected virtual void Dispose(bool disposing)
+    {
+        if (disposedValue)
+        {
+            return;
+        }
+
+        if (disposing)
+        {
+            testDbContext.Dispose();
+        }
+
+        disposedValue = true;
     }
 
     /// <summary>
     /// Create valid entity test.
     /// </summary>
     [Fact]
-    [TestOrder(1)]
     public async Task CreateEntity_ValidEntity_Success()
     {
         // Arrange
@@ -53,7 +72,6 @@ public class CreateEntityTests : TestBed<NetForgeFixture>
     /// Create already existed entity test.
     /// </summary>
     [Fact]
-    [TestOrder(2)]
     public async Task CreateEntity_AlreadyExistingEntity_Error()
     {
         // Arrange
