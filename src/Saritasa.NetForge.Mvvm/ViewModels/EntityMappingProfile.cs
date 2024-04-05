d614f7894a97feae3f073288b418b9b1b375115f
Alexander Makarov <alexandr.makarov@saritasa.com>
2024-04-05 14:47:08 +0700

    Replace entity details model mapping SN-641

--- src/Saritasa.NetForge.Mvvm/ViewModels/EntityMappingProfile.cs
+++ src/Saritasa.NetForge.Mvvm/ViewModels/EntityMappingProfile.cs
@@ -1,7 +1,6 @@
 ﻿using AutoMapper;
 using Saritasa.NetForge.Mvvm.ViewModels.CreateEntity;
 using Saritasa.NetForge.Mvvm.ViewModels.EditEntity;
-using Saritasa.NetForge.Mvvm.ViewModels.EntityDetails;
 using Saritasa.NetForge.UseCases.Metadata.GetEntityById;
 
 namespace Saritasa.NetForge.Mvvm.ViewModels;
@@ -16,9 +15,6 @@ internal class EntityMappingProfile : Profile
     /// </summary>
     public EntityMappingProfile()
     {
-        CreateMap<GetEntityByIdDto, EntityDetailsModel>()
-            .ForMember(model => model.StringId, options => options.Ignore())
-            .ForMember(model => model.Navigations, options => options.Ignore());
         CreateMap<GetEntityByIdDto, CreateEntityModel>()
             .ForMember(model => model.EntityInstance, options => options.Ignore());
         CreateMap<GetEntityByIdDto, EditEntityModel>()
