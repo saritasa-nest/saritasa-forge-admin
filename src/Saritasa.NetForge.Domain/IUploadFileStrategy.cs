3104ac8da25d5aa0fc982febdaa05f2daf8eee99
Alexandr Makarov <Alexandr.Makarov@saritasa.com>
2024-05-06 11:07:04 +0700

    Update comments in IUploadFileStrategy for clarity SN-603

--- src/Saritasa.NetForge.Domain/IUploadFileStrategy.cs
+++ src/Saritasa.NetForge.Domain/IUploadFileStrategy.cs
@@ -12,13 +12,19 @@ public interface IUploadFileStrategy
     /// </summary>
     /// <param name="file">File to upload.</param>
     /// <param name="cancellationToken">Token to cancel the operation.</param>
-    /// <returns>File related object.</returns>
+    /// <returns>
+    /// Object that represents uploaded file.
+    /// For example, when using file system, it can be path to uploaded file.
+    /// </returns>
     Task<object> UploadFileAsync(IBrowserFile file, CancellationToken cancellationToken);
 
     /// <summary>
     /// Gets file source.
     /// </summary>
-    /// <param name="fileString">File related string.</param>
-    /// <returns>String that represents file source.</returns>
+    /// <param name="fileString">
+    /// String that helps with getting file source.
+    /// For example: when using S3, it will contain file URI that will be used to generate pre signed URL.
+    /// </param>
+    /// <returns>File source that gives access to a file.</returns>
     string GetFileSource(string fileString);
 }
