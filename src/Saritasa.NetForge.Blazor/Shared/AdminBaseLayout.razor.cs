dc87bba8ef62d9d48547dd414214cd05f61aad2e
Vasily Chefonov <81791194+vasiliy-chefonov@users.noreply.github.com>
2024-10-10 14:55:04 +0700

    Add the base layout component SN-755

---
+++ src/Saritasa.NetForge.Blazor/Shared/AdminBaseLayout.razor.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Components;
+
+namespace Saritasa.NetForge.Blazor.Shared;
+
+/// <summary>
+/// Base layout for admin pages.
+/// </summary>
+public partial class AdminBaseLayout : LayoutComponentBase;
+
