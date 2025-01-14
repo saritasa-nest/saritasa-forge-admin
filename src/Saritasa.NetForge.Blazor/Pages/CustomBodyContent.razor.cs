997b52c1874b8450362d54bada57f45c4eb1e5d0
Alexandr Makarov <Alexandr.Makarov@saritasa.com>
2025-01-14 13:46:26 +0700

    Remove CustomBodyContent admin component SN-756

--- src/Saritasa.NetForge.Blazor/Pages/CustomBodyContent.razor.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Components;
-
-namespace Saritasa.NetForge.Blazor.Pages;
-
-public partial class CustomBodyContent
-{
-    [Parameter]
-    public RenderFragment? Content { get; set; }
-}
